fcd3d3d6fb08e93d4d7a586aaed3b5e59206f15f
huanglei <1642331764@qq.com>
2022-06-02 18:39:41 +0800

    6.2 commit

---
+++ Simple/src/SingleNumber_136.java
@@ -0,0 +1,18 @@
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 说明：你的算法应该具有线性时间复杂度。
+ */
+public class SingleNumber_136 {
+    public static int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+}
