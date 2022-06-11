f28218cc91c7dcd36920008eb1352de88ff81bc5
huanglei <1642331764@qq.com>
2022-06-11 11:07:41 +0800

    6.11 commit

---
+++ Simple/src/InvertTree_226.java
@@ -0,0 +1,51 @@
+/**
+ * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+ */
+public class InvertTree_226 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" +
+                    "val=" + val +
+                    ", left=" + left +
+                    ", right=" + right +
+                    '}';
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = left;
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4,
+                new TreeNode(2,
+                        new TreeNode(1),
+                        new TreeNode(3)),
+                new TreeNode(7,
+                        new TreeNode(6),
+                        new TreeNode(9)));
+        System.out.println(invertTree(root));
+    }
+}
