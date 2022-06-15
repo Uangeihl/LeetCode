83db3c68903c152b319c540d252e4a92e1ba984a
huanglei <1642331764@qq.com>
2022-06-15 11:13:43 +0800

    6.15 commit

---
+++ Simple/src/ContainsNearbyDuplicate_219.java
@@ -0,0 +1,29 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ */
+public class ContainsNearbyDuplicate_219 {
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        /*for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+                if (nums[i] == nums[j]) return true;
+            }
+        }
+        return false;*/
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) return true;
+            else map.put(nums[i], i);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+}
