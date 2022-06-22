bdf02d5c977b339a5aff70ca91aaa88bd9a66015
huanglei <1642331764@qq.com>
2022-06-22 15:10:47 +0800

    6.22 commit

---
+++ Simple/src/LowestCommonAncestor_235.java
@@ -0,0 +1,43 @@
+/**
+ * 给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ */
+
+public class LowestCommonAncestor_235 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q);
+        else if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q);
+        else return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6,
+                new TreeNode(2,
+                        new TreeNode(0),
+                        new TreeNode(4,
+                                new TreeNode(3),
+                                new TreeNode(5))),
+                new TreeNode(8,
+                        new TreeNode(7),
+                        new TreeNode(9)));
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(8);
+        System.out.println(lowestCommonAncestor(root, p, q).val);
+    }
+}
