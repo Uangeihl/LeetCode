e72c82a2bbb8e51a345a4b906ba392ea3564d9a5
huanglei <1642331764@qq.com>
2022-06-27 14:24:05 +0800

    6.27 commit

---
+++ Simple/src/IsUgly_263.java
@@ -0,0 +1,18 @@
+/**
+ * 丑数 就是只包含质因数 2、3 和 5 的正整数。
+ * 给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+ */
+public class IsUgly_263 {
+    public static boolean isUgly(int n) {
+        if (n <= 0) return false;
+        if (n == 1) return true;
+        else if (n % 2 == 0) return isUgly(n / 2);
+        else if (n % 3 == 0) return isUgly(n / 3);
+        else if (n % 5 == 0) return isUgly(n / 5);
+        else return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isUgly(13));
+    }
+}
