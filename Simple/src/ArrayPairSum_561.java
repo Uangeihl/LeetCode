0354be4d9b2fd14dcd3054798078e0a32b4af411
huanglei <1642331764@qq.com>
2022-08-17 17:08:51 +0800

    8.17 commit

---
+++ Simple/src/ArrayPairSum_561.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+/**
+ * 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+ * 返回该 最大总和 。
+ */
+public class ArrayPairSum_561 {
+    public static int arrayPairSum(int[] nums) {
+        int sum = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {6, 2, 6, 5, 1, 2};
+        System.out.println(arrayPairSum(nums));
+    }
+}
