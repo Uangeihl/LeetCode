db9721004f18335464b413a42e26f3cba544777f
huanglei <1642331764@qq.com>
2022-08-25 15:13:47 +0800

    8.25 commit

---
+++ Simple/src/Postorder_590.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个 n 叉树的根节点 root ，返回 其节点值的 后序遍历 。
+ * n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+ */
+public class Postorder_590 {
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        help_590(root, res);
+        return res;
+    }
+
+    static void help_590(Node root, List<Integer> res) {
+        if (root != null) {
+            for (Node child : root.children) {
+                help_590(child, res);
+            }
+            res.add(root.val);
+        }
+    }
+}
