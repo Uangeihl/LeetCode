eece1dd93e9cd2080d6a436da98be976006e5c01
huanglei <1642331764@qq.com>
2022-09-09 22:29:07 +0800

    9.9 commit

---
+++ Simple/src/HasAlternatingBits_693.java
@@ -0,0 +1,21 @@
+/**
+ * 给定一个正整数，检查它的二进制表示是否总是 0、1 交替出现：换句话说，就是二进制表示中相邻两位的数字永不相同。
+ */
+public class HasAlternatingBits_693 {
+    public static boolean hasAlternatingBits(int n) {
+        int prev = 2;
+        while (n != 0) {
+            int cur = n % 2;
+            if (cur == prev) {
+                return false;
+            }
+            prev = cur;
+            n /= 2;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasAlternatingBits(5));
+    }
+}
