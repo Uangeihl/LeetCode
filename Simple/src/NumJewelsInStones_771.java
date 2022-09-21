3c2dd2bf5f31be534e138e760523d962421d2ab1
huanglei <1642331764@qq.com>
2022-09-21 08:49:06 +0800

    9.21 commit

---
+++ Simple/src/NumJewelsInStones_771.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *  给你一个字符串 jewels 代表石头中宝石的类型，另有一个字符串 stones 代表你拥有的石头。 stones 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+ * 字母区分大小写，因此 "a" 和 "A" 是不同类型的石头。
+ */
+public class NumJewelsInStones_771 {
+    public static int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+        int count = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (set.contains(stones.charAt(i))) count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numJewelsInStones("aA", "aAAbbbb"));
+    }
+}
