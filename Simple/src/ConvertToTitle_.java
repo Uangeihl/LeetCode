151a62d5d07d3647a0ee7e60fab618b52fb33dff
Uangeihl <1642331764@qq.com>
2022-09-25 12:36:40 +0800

    9.25 commit

---
+++ Simple/src/ConvertToTitle_.java
@@ -0,0 +1,17 @@
+/**
+ * 给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+ */
+public class ConvertToTitle_ {
+    public static String convertToTitle(int columnNumber) {
+        StringBuffer res = new StringBuffer();
+        while (columnNumber > 0) {
+            res.append(Character.toChars('A' + columnNumber % 26 - 1));
+            columnNumber /= 26;
+        }
+        return String.valueOf(res.reverse());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToTitle(701));
+    }
+}
