bc6e5e69521f32be6b7175750c61e63f3571fe1d
huanglei <1642331764@qq.com>
2022-09-30 09:53:41 +0800

    9.30 commit

---
+++ Simple/src/Transpose_867.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+/**
+ * 给你一个二维整数数组 matrix， 返回 matrix 的 转置矩阵 。
+ * 矩阵的 转置 是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+ */
+public class Transpose_867 {
+    public static int[][] transpose(int[][] matrix) {
+        int[][] res = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
+        System.out.println(Arrays.deepToString(transpose(matrix)));
+    }
+}
