96c294546cf38f4c5809aa4a1d222afbf2c32807
huanglei <1642331764@qq.com>
2022-11-15 09:17:51 +0800

    11.15 commit

---
+++ Simple/src/IsUnivalTree_965.java
@@ -0,0 +1,32 @@
+/**
+ * 如果二叉树每个节点都具有相同的值，那么该二叉树就是单值二叉树。
+ * 只有给定的树是单值二叉树时，才返回 true；否则返回 false。
+ */
+
+public class IsUnivalTree_965 {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isUnivalTree(TreeNode root) {
+        if (root == null) return true;
+        if (root.left != null && root.left.val != root.val) return false;
+        if (root.right != null && root.right.val != root.val) return false;
+        return isUnivalTree(root.left) && isUnivalTree(root.right);
+    }
+}
