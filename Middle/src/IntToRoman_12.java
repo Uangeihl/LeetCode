75dab05b16c67f55cd02a7cad210cfef7be00f03
huanglei <1642331764@qq.com>
2022-11-24 13:25:03 +0800

    11.24 commit

--- Middle/src/IntToRoman_12.java
+++ Middle/src/IntToRoman_12.java
@@ -16,6 +16,19 @@
  * 给你一个整数，将其转为罗马数字。
  */
 public class IntToRoman_12 {
+    /*int[] values = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (num != 0) {
+            while (values[i] > num) i++;
+            sb.append(symbols[i]);
+            num -= values[i];
+        }
+        return sb.toString();
+    }*/
     public static String intToRoman(int num) {
         if (num / 1000 > 0) return "M" + intToRoman(num - 1000);
         else if (num / 900 > 0) return "CM" + intToRoman(num - 900);
---
+++ Middle/src/ThreeSum_15.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+/**
+ * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+ * 你返回所有和为 0 且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ */
+public class ThreeSum_15 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 枚举 a
+        for (int first = 0; first < n; ++first) {
+            // 需要和上一次枚举的数不相同
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            // c 对应的指针初始指向数组的最右端
+            int third = n - 1;
+            int target = -nums[first];
+            // 枚举 b
+            for (int second = first + 1; second < n; ++second) {
+                // 需要和上一次枚举的数不相同
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                // 需要保证 b 的指针在 c 的指针的左侧
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 如果指针重合，随着 b 后续的增加
+                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> list = new ArrayList<Integer>();
+                    list.add(nums[first]);
+                    list.add(nums[second]);
+                    list.add(nums[third]);
+                    ans.add(list);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum_15 test = new ThreeSum_15();
+        System.out.println(test.threeSum(new int[]{-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6}));
+    }
+}
