d0c5403a2b9ac967f03efdfdac4d95c3467c62e9
huanglei <1642331764@qq.com>
2022-11-29 10:54:27 +0800

    11.29 commit

---
+++ Middle/src/GenerateParenthesis_22.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ */
+public class GenerateParenthesis_22 {
+    List<String> res = new ArrayList<>();
+
+    public List<String> generateParenthesis(int n) {
+        if (n <= 0) return res;
+        getParenthesis("", n, n);
+        return res;
+    }
+
+    private void getParenthesis(String str, int left, int right) {
+        if (left == 0 && right == 0) {
+            res.add(str);
+            return;
+        }
+        if (left == right) {
+            //剩余左右括号数相等，下一个只能用左括号
+            getParenthesis(str + "(", left - 1, right);
+        } else if (left < right) {
+            //剩余左括号小于右括号，下一个可以用左括号也可以用右括号
+            if (left > 0) getParenthesis(str + "(", left - 1, right);
+            getParenthesis(str + ")", left, right - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        GenerateParenthesis_22 s = new GenerateParenthesis_22();
+        System.out.println(s.generateParenthesis(2));
+    }
+}
